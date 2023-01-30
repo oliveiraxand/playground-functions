a62bc40f5a9eaa60e8c8d1866203ed02ef61f5a9
Alexandre Oliveira <alexandre.osantos253@gmail.com>
2023-01-30 17:34:10 -0300

    Desafio 11 Supostamente Completo

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,26 @@
 // Desafio 11 - Crie a função generatePhoneNumber
+const generatePhoneNumber = (arr) =>{
+  let erroMsg ="não é possível gerar um número de telefone com esses valores";
+  //Verifica se é diferente de 11
+  if(arr.length !== 11){
+    return erroMsg;
+  }
+  //Verifica se o mesmo numero se repete 3x ou 
+  let count = 0;
+  for(let v of arr){
 
+  }
+  //verifica se existe um numero menor que 0 ou maior que 9
+  for(let v of arr){
+    if(v>9 || v<0){
+      return erroMsg;
+    }
+  }
+  //**TEMPLATE LITERALS** (12) 34567-8901
+  return `(${arr[0]}${arr[1]}) ${arr[2]}${arr[3]}${arr[4]}${arr[5]}${arr[6]}-${arr[7]}${arr[8]}${arr[9]}${arr[10]}`;
+
+}
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 // Desafio 12 -  Crie a função triangleCheck
 
 // Desafio 13 - Crie a função hydrate
